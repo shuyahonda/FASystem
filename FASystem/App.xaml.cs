3ef2f96cdd7b315b103fe168aa4a6f27014b7ba9
Shuya Honda <syv2357@gmail.com>
2015-12-14 15:49:36 +0900

    アプリの起動時と終了時をハンドリング

--- FASystem/App.xaml.cs
+++ FASystem/App.xaml.cs
@@ -13,5 +13,22 @@ namespace FASystem
     /// </summary>
     public partial class App : Application
     {
+        /// <summary>
+        /// アプリケーションが終了する時のイベント。
+        /// </summary>
+        /// <param name="e">イベント データ。</param>
+        protected override void OnExit(ExitEventArgs e)
+        {
+        }
+
+
+        /// <summary>
+        /// アプリケーションが開始される時のイベント。
+        /// </summary>
+        /// <param name="e">イベント データ。</param>
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            Console.WriteLine("OnStartup");
+        }
     }
 }
